4c725b0dda7350caa299764fbd04ac9c49d136b4
Carlos Espinoza <cespinoza@plannerapp.net>
2023-02-13 21:24:54 -0600

    cambios para el tema de URL

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
 import IShopingCart from "./utils/interface/localstorage/IShopingCart";
 
 const env = {
-    API_URL: "https://localhost:44368",
+    // API_URL: "https://localhost:44368",
     API_MAPS_KEY: "AIzaSyCbNiuwg2ODvUYr5606E8CUd_YTRDnma1w",
-    // API_URL: 'https://hilos-perlas.azurewebsites.net',
+    API_URL: 'https://hilos-perlas.azurewebsites.net',
 
     company: {
         name: "De hilos y perlas",
