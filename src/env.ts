a5cec8f0ccd695a49afba7836a994181e23db34a
Carlos Espinoza <cespinoza@plannerapp.net>
2023-02-23 01:27:26 -0600

    se logro terminar esto

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,8 @@ import IShopingCart from "./utils/interface/localstorage/IShopingCart";
 const env = {
     // API_URL: "https://localhost:44368",
     API_MAPS_KEY: "AIzaSyCbNiuwg2ODvUYr5606E8CUd_YTRDnma1w",
-    API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
+    // API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
+    API_URL: 'https://734e-143-202-253-244.ngrok.io',
 
     company: {
         name: "De hilos y perlas",
