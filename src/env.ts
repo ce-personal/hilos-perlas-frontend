a2da3fb284fdc23659bf99f60ccb012b2c339988
Carlos Espinoza <cespinoza@plannerapp.net>
2023-02-22 23:45:15 -0600

    se cambia a produccion

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import IShopingCart from "./utils/interface/localstorage/IShopingCart";
 
 const env = {
     // API_URL: "https://localhost:44368",
-    // API_MAPS_KEY: "AIzaSyCbNiuwg2ODvUYr5606E8CUd_YTRDnma1w",
+    API_MAPS_KEY: "AIzaSyCbNiuwg2ODvUYr5606E8CUd_YTRDnma1w",
     API_URL: 'https://hilos-perlas.azurewebsites.net',
 
     company: {
