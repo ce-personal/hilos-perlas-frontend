36984e8066f6ae08ed602838a13bb984c53168a7
Carlos Espinoza <cespinoza@plannerapp.net>
2023-03-10 22:26:37 -0600

    cambiando url

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,8 @@ import IShopingCart from "./utils/interface/localstorage/IShopingCart";
 
 const env = {
     API_MAPS_KEY: "AIzaSyCbNiuwg2ODvUYr5606E8CUd_YTRDnma1w",
-    API_URL: "https://localhost:44368",
-    // API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
+    // API_URL: "https://localhost:44368",
+    API_URL: 'https://de-hilos-y-perlas.azurewebsites.net',
     // API_URL: 'https://734e-143-202-253-244.ngrok.io',
 
     company: {
